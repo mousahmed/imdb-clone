b57df79c6848ad15756cb164389d94fce4f7da6b
Moustafa Mansour <mous.ahmed@outlook.com>
2023-10-06 16:23:14 +0200

    Added loading effect

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="flex justify-center">
+      <img className="h-96" src="spinner.svg" alt="loading..." />
+    </div>
+  );
+}
+
+export default Loading;
